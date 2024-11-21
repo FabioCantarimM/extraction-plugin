d726f038c643d8914a6eeafc5ad841a0dc9825c0
Fabio Cantarim <fabio.cantarim@gmail.com>
2024-11-21 09:33:04 -0400

    adding categoria

--- API/main.js
+++ API/main.js
@@ -72,6 +72,26 @@ app.get('/api/concorrente/:sku', async (req, res) =>{
 
 })
 
+app.get('/api/categoria/:cat', async (req, res) =>{
+  const { cat } = req.params;
+  try {
+      console.log('Categoria recebida:', cat); // Log do SKU recebido
+      const result = await pool.query('SELECT total_rbv_l1m FROM drogadata WHERE "CATEGORIA" = $1', [cat]);
+
+      // Verifica se algum produto foi encontrado
+      if (result.rows.length === 0) {
+        return res.status(404).json({ error: 'Categoria não encontrada' });
+      }
+
+      // Retorna os dados do produto encontrado
+      return res.json(result.rows[0]); // Retorna o primeiro produto encontrado
+  } catch (error) {
+    console.error('Erro ao acessar o banco de dados:', error);
+    res.status(500).send('Erro ao acessar o banco de dados');
+  }
+
+})
+
 
 app.listen(port, () => {
     const ip = getLocalIP()
